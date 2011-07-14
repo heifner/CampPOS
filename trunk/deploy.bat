0e47cc0071e16d9d84321f9e68211921086c94e3
heifner <heifner@99a70a5b-54c6-47f1-a755-3e8855cde8af>
2011-07-14 00:42:08 +0000

    -Updated deploy.bat to include Visual Studio redistribution dlls.  - Needed for one of the computers that would not work without them.

--- trunk/deploy.bat
+++ trunk/deploy.bat
@@ -3,6 +3,7 @@ REM - TARGET_DIR and CampPOS.zip are deleted.
 REM ----------------------------------------- 
 
 set DEPLOY_DIR=CampPOS
+set VC_REDIST_DIR=C:\Program Files\Microsoft Visual Studio 8\VC\redist\x86\Microsoft.VC80.CRT
 set TARGET_DIR=C:\temp\%DEPLOY_DIR%
 set CURRENT_DIR=%CD%
 
@@ -14,6 +15,8 @@ mkdir %TARGET_DIR%
 
 copy Output\Release\*.exe %TARGET_DIR%
 copy Output\Release\*.dll %TARGET_DIR%
+copy "%VC_REDIST_DIR%"\*.dll %TARGET_DIR%
+copy "%VC_REDIST_DIR%"\*.manifest %TARGET_DIR%
 copy CampPOS.ini %TARGET_DIR%
 
 copy %BSAPI_ROOT%\bin\32-bit\bsapi.dll %TARGET_DIR%
