20197016a65626b8a6f434029c25f1a763ba8090
heifner <heifner@99a70a5b-54c6-47f1-a755-3e8855cde8af>
2018-07-01 21:35:09 +0000

    Updates for Win10 Machine

--- trunk/go.bat
+++ trunk/go.bat
@@ -1,4 +1,4 @@
-set CAMPPOS_ROOT=D:\heifner\CampPOS\trunk
+set CAMPPOS_ROOT=C:\dev\heifner\CampPOS\trunk
 set MPC_ROOT=D:\dev_tools\MPC-MPC_4_1_2
 set BSAPI_ROOT=%CAMPPOS_ROOT%\bsapi
 set PATH=%MPC_ROOT%;%PATH%;%BSAPI_ROOT%\bin\32-bit
